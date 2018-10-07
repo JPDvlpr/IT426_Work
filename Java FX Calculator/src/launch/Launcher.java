62388c8f27aace7f28bca637a37291f9374a4de1
JPDvlpr <j.pappoe@hotmail.com>
2018-10-07 16:05:53 -0700

    Work with me guthub

--- Java FX Calculator/src/launch/Launcher.java
+++ Java FX Calculator/src/launch/Launcher.java
@@ -3,5 +3,6 @@ package launch;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("testing launcher");
+        System.out.println("cmon github");
     }
 }

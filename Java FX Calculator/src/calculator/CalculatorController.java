736dd9216ae2403a1172af09618695724cd611c8
JPDvlpr <j.pappoe@hotmail.com>
2018-10-15 15:58:49 -0700

    transfered the load from controller to model

--- Java FX Calculator/src/calculator/CalculatorController.java
+++ Java FX Calculator/src/calculator/CalculatorController.java
@@ -10,52 +10,15 @@ import model.CalculatorModel;
 import java.text.DecimalFormat;
 
 public class CalculatorController {
-    private String savedCalcInput;
-    private String currentCalcInput;
-    private String operators = "+ - * / Enter C";
-    private String operatorInput = "";
-    DecimalFormat df = new DecimalFormat("#.00"); // Set your desired format here.
+
 
     private CalculatorModel model = new CalculatorModel();
 
     public void calcInput(String input) {
-
-        if (operatorInput == "C") {
-            currentCalcInput = "";
-            savedCalcInput = "";
-            operatorInput = "";
-            input = "";
-        }
-
-        if (operators.contains(input)) {
-            if (operatorInput == "") {
-                operatorInput = input;
-                savedCalcInput = currentCalcInput;
-                currentCalcInput = "";
-            } else if (operatorInput == "Enter") {
-                //savedCalcInput = String.valueOf(model.calculate(Integer.parseInt(savedCalcInput), Integer.parseInt(currentCalcInput), operatorInput));
-                operatorInput = input;
-                currentCalcInput = "";
-//            } //else if (operatorInput == "/") {
-//                savedCalcInput = df.format(Integer.parseInt(savedCalcInput) / 100.0);
-            } else {
-                savedCalcInput = "" + model.calculate(Integer.parseInt(savedCalcInput), Integer.parseInt(currentCalcInput), operatorInput);
-                operatorInput = input;
-                currentCalcInput = "";
-            }
-            //if theres nothing in calc then set curr to input
-        } else if (currentCalcInput == "" || currentCalcInput == null) {
-            currentCalcInput = input;
-        } else {
-            currentCalcInput += input;
-        }
-        System.out.println("cci: " + currentCalcInput + "oi: " + operatorInput + "sci: " + savedCalcInput + " " + input);
+        model.calcInput(input);
     }
 
     public String showResult() {
-        if (operatorInput == "Enter") {
-            return savedCalcInput;
-        }
-        return currentCalcInput;
+        return model.showResult();
     }
 }

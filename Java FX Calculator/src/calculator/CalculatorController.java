7e6dbbb702e0007f4c5425f995649e5fe5408546
JPDvlpr <j.pappoe@hotmail.com>
2018-10-16 23:53:05 -0700

    Cleaning code and working with css

--- Java FX Calculator/src/calculator/CalculatorController.java
+++ Java FX Calculator/src/calculator/CalculatorController.java
@@ -1,16 +1,8 @@
 package calculator;
 
-import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import model.CalculatorModel;
 
-import java.text.DecimalFormat;
-
 public class CalculatorController {
-
 
     private CalculatorModel model = new CalculatorModel();
 
--- Java FX Calculator/src/ui/CalculatorUI.java
+++ Java FX Calculator/src/ui/CalculatorUI.java
@@ -9,30 +9,38 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
- * Added an example of using the GridPane layout.
+ * Calculator user interface. Displays the view
+ * for the user to interact and process calculations
  *
- * @author Josh Archer
+ * @author Jhakon Pappoe
  * @version 1.0
  */
 public class CalculatorUI extends Application {
-    public static final int NUM_COLS = 4;
-    public static final int COL_WIDTH = 40;
-    public static final int WIN_WIDTH = COL_WIDTH * 7;
-    public static final int WIN_HEIGHT = COL_WIDTH * 7;
-    private static final double BUTTON_WIDTH = 32;
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int COLSPAN = 2;
+    private static final int NUM_COLS = 4;
+    private static final int COL_WIDTH = 40;
+    private static final int ROW_INDEX = 5;
+    private static final int ROWSPAN = 1;
+    private static final int WIN_WIDTH = COL_WIDTH * 7;
+    private static final int WIN_HEIGHT = COL_WIDTH * 7;
+    private static final double BUTTON_WIDTH = 40;
+    private static final int BUTTON_PADDING = 10;
     CalculatorController controller = new CalculatorController();
 
+    /**
+     * starts the stage, sets title, adds stylesheet
+     * @param stage
+     */
     @Override
     public void start(Stage stage) {
-
         Scene scene = getScene();
         scene.getStylesheets().add("styles/styles.css");
-
-        stage.setTitle("Working in a grid");
+        stage.setTitle("The Most Basic Calculator");
         stage.setScene(scene);
         stage.show();
     }
@@ -41,15 +49,10 @@ public class CalculatorUI extends Application {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setId("grid");
+        grid.setHgap(BUTTON_PADDING);
+        grid.setVgap(BUTTON_PADDING);
+        grid.setPadding(new Insets(BUTTON_PADDING));
 
-        //set some spacing
-        grid.setHgap(10);
-        grid.setVgap(10);
-
-        //grid.setGridLinesVisible(true);
-        grid.setPadding(new Insets(10));
-
-        //we can define the size of columns or rows
         ObservableList<ColumnConstraints> cols = grid.getColumnConstraints();
 
         for (int i = 1; i <= NUM_COLS; i++) {
@@ -64,9 +67,9 @@ public class CalculatorUI extends Application {
                 new Button("-"), new Button("1"), new Button("2"), new Button("3"),
                 new Button("*"), new Button("0"), new Button("Enter"), new Button("/"), new Button("C")};
 
-        int counter = 0;
+        int counter = ZERO;
 
-        for (int i = 0; i < buttons.length; i++) {
+        for (int i = ZERO; i < buttons.length; i++) {
 
             int columns = i % NUM_COLS;
             int rows = i / NUM_COLS;
@@ -78,8 +81,8 @@ public class CalculatorUI extends Application {
             });
 
             if (buttons[i].getText().equals("Enter")) {
-                grid.add(buttons[i], columns, rows, 2, 1);
-                buttons[i].setPrefWidth(COL_WIDTH * 2);
+                grid.add(buttons[i], columns, rows, COLSPAN, ONE);
+                buttons[i].setPrefWidth(COL_WIDTH * COLSPAN + BUTTON_PADDING);
                 counter++;
             } else {
                 grid.add(buttons[i], columns + counter, rows);
@@ -87,7 +90,7 @@ public class CalculatorUI extends Application {
             }
         }
 
-        grid.add(total, 0, 5, NUM_COLS, 1);
+        grid.add(total, ZERO, ROW_INDEX, NUM_COLS, ROWSPAN);
 
         return new Scene(grid, WIN_WIDTH, WIN_HEIGHT);
     }

5a40ed485ce3828b42031085357676adab967c3b
JPDvlpr <j.pappoe@hotmail.com>
2018-10-26 09:43:02 -0700

    fixed controlller to accept all note input based on table name and 2 params, QuotesNotes works, HyperlinkNotes works. Changed function in the interface

--- NoteTakingApp/src/model/DBTest.java
+++ NoteTakingApp/src/model/DBTest.java
@@ -3,7 +3,7 @@ package model;
 public class DBTest {
     public static void main(String[] args) {
         DBNotes db = new DBNotes();
-        db.addQuote("Hello ppl", "Melanie");
+        //db.addQuote("Hello ppl", "Melanie");
 
     }
 }
